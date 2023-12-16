cbb2de140039e745184d731aa42daa5f991fe07f
webmiskin <149207854+webmiskin@users.noreply.github.com>
2023-12-17 04:33:13 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+
+// Scroll to top
+const scrollToTop = document.querySelector('.scroll-to-top')
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTop.style.display = "grid"
+    } else {
+        scrollToTop.style.display = "none"
+    }
+});
+
+
+
+// Aos
+
+    AOS.init({
+    	duration: 1000,
+    	easing:"ease-in-out",
+    });
+
+
+ // Slider
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 24,
+    speed: 1000,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+  });
